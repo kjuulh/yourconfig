25472a96d54ec28646a47aade50ddc1ffc24a6a1
kjuulh <contact@kasperhermansen.com>
2025-07-21 16:35:27 +0200

    feat: add basic config loader

---
+++ config.go
@@ -0,0 +1,108 @@
+package yourconfig
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/ettle/strcase"
+)
+
+func MustLoad[T any]() T {
+	output, err := Load[T]()
+	if err != nil {
+		panic(fmt.Sprintf("must load: %s", err.Error()))
+	}
+
+	return output
+}
+
+func Load[T any]() (T, error) {
+	var cfg T
+
+	v := reflect.ValueOf(&cfg).Elem()
+	t := v.Type()
+
+	errs := make([]error, 0)
+
+OUTER:
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tagStr := field.Tag.Get("conf")
+		if tagStr == "" {
+			continue
+		}
+
+		singleValue := make([]string, 0)
+		options := make(map[string]string, 0)
+
+		values := strings.SplitSeq(tagStr, ",")
+		for value := range values {
+			if value == "" {
+				continue
+			}
+
+			key, val, ok := strings.Cut(strings.TrimSpace(value), ":")
+			if ok {
+				options[key] = val
+			} else {
+				singleValue = append(singleValue, value)
+			}
+		}
+
+		var tag tag
+		if len(singleValue) == 0 {
+			tag.Env = strcase.ToSNAKE(field.Name)
+		} else {
+			tag.Env = singleValue[0] // We always count the first value as the name, if set
+		}
+
+		for _, option := range singleValue {
+			switch option {
+			case "required":
+				tag.Required = true
+			}
+		}
+
+		for key, val := range options {
+			switch key {
+			case "required":
+				required, err := strconv.ParseBool(val)
+				if err != nil {
+					errs = append(errs, fmt.Errorf("field: %s (key: %s), err: %w", field.Name, key, err))
+					continue OUTER
+				}
+
+				tag.Required = required
+			}
+		}
+
+		valueStr := os.Getenv(tag.Env)
+		if valueStr == "" && tag.Required {
+			errs = append(errs, fmt.Errorf("field: %s (env=%s) is not set and is required", field.Name, tag.Env))
+			continue OUTER
+		}
+
+		fieldValue := v.Field(i)
+		if !fieldValue.CanSet() {
+			errs = append(errs, fmt.Errorf("field: %s is not settable", field.Name))
+			continue OUTER
+		}
+
+		fieldValue.SetString(valueStr)
+	}
+
+	if err := errors.Join(errs...); err != nil {
+		return cfg, fmt.Errorf("config failed: %w", err)
+	}
+
+	return cfg, nil
+}
+
+type tag struct {
+	Env      string
+	Required bool
+}
---
+++ config_test.go
@@ -0,0 +1,168 @@
+package yourconfig_test
+
+import (
+	"testing"
+
+	"github.com/kjuulh/yourconfig"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoad(t *testing.T) {
+	t.Run("no types", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Zero(t, val)
+	})
+
+	t.Run("default tag, nothing set, no env set", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:""`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Zero(t, val)
+	})
+
+	t.Run("default tag (required=true), nothing set, no env set, err", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"required:true"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.Error(t, err)
+		require.Zero(t, val)
+
+		assert.Equal(t, "config failed: field: SomeItem (env=SOME_ITEM) is not set and is required", err.Error())
+	})
+
+	t.Run("default tag (required=false), nothing set, no env set no error", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"required:false"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Zero(t, val)
+	})
+
+	t.Run("env tag nothing set, no env set, no error", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"SOME_ITEM"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Zero(t, val)
+	})
+
+	t.Run("default tag (required=true), nothing set, no env set, err", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"SOME_ITEM,required:true"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.Error(t, err)
+		require.Zero(t, val)
+
+		assert.Equal(t, "config failed: field: SomeItem (env=SOME_ITEM) is not set and is required", err.Error())
+	})
+
+	t.Run("default tag (required), nothing set, no env set, err", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"SOME_ITEM,required"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		val, err := yourconfig.Load[Config]()
+		require.Error(t, err)
+		require.Zero(t, val)
+
+		assert.Equal(t, "config failed: field: SomeItem (env=SOME_ITEM) is not set and is required", err.Error())
+	})
+
+	t.Run("default tag private, trying to set, err", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string
+			someOtherItem string `conf:"required:true"`
+			someBool      bool
+		}
+
+		t.Setenv("SOME_OTHER_ITEM", "unsettable")
+
+		val, err := yourconfig.Load[Config]()
+		require.Error(t, err)
+		require.Zero(t, val)
+
+		assert.Equal(t, "config failed: field: someOtherItem is not settable", err.Error())
+	})
+
+	t.Run("env tag and env set, no error", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"required:true"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		t.Setenv("SOME_ITEM", "some-item")
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Equal(t, "some-item", val.SomeItem)
+	})
+
+	t.Run("env tag (different name) and env set, no error", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"DIFFERENT_NAME,required:true"`
+			someOtherItem string
+			someBool      bool
+		}
+
+		t.Setenv("DIFFERENT_NAME", "some-item")
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Equal(t, "some-item", val.SomeItem)
+	})
+
+	t.Run("multiple env tag and env set, no error", func(t *testing.T) {
+		type Config struct {
+			SomeItem      string `conf:"required:true"`
+			SomeOtherItem string `conf:"required:true"`
+			someBool      bool
+		}
+
+		t.Setenv("SOME_ITEM", "some-item")
+		t.Setenv("SOME_OTHER_ITEM", "some-other-item")
+
+		val, err := yourconfig.Load[Config]()
+		require.NoError(t, err)
+
+		assert.Equal(t, "some-item", val.SomeItem)
+		assert.Equal(t, "some-other-item", val.SomeOtherItem)
+	})
+}
